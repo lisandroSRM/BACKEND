b91f01c945b4140c4f675fe09b1e5aabdbed67b6
lisandroSRM <solislisandro9@gmail.com>
2024-02-01 14:28:09 -0600

    coneccion al bd

--- index.php
+++ index.php
@@ -3,4 +3,18 @@ require_once realpath('./vendor/autoload.php');
     $dotenv=Dotenv\Dotenv::createImmutable('./');
     $dotenv->load();
     echo $_ENV['MI_VARIABLE_DE_ENTORNO'];
+    $puerto = $_ENV['PUERTO'];
+    $usuario = $_ENV['USUARIO'];
+    $password = $_ENV['PASSWORD'];
+    $bd = $_ENV['BD'];
+    $host= $_ENV['HOST'];
+
+
+    $conexion = new PDO("mysql:host=$host;port=$puerto;dbname=$bd",$usuario,$password);
+
+    if ($conexion != True) {
+        echo "CONEXION FALLIDA";
+    }else {
+        echo "CONEXION EXITOSA";
+    }
 ?>
